98bb2f16eec4717d33480fca98c5340caa0e67dc
Rayna Schexnayder <Rayna.schexnayder@gmail.com>
2019-10-24 17:34:13 -0500

    Complete

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,10 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      if(!this.movies.includes(newTitle) && newTitle !== " " && newTitle !==""){
+         this.movies.push(newTitle);
+      }    
+   }
 }
